8b5c4ea80c9e1b3756cbfc56db80418b1ec992c5
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2021-07-27 16:33:44 +0300

    interim commit

--- R/import.R
+++ R/import.R
@@ -37,8 +37,9 @@
 #' numeric score (e.g. using fraction of the category).
 #' @param metadata can be either a data frame with a column named "metacell" with the metacell id and other metadata columns
 #' or a name of a delimited file which contains such data frame.
-#' @param metadata_colors a named list with colors for each metadata column.
-#' Colors should be given as a list where the first element is a vector of breaks and the second element is a vector of colors.
+#' @param metadata_colors a named list with colors for each metadata column, or a name of a yaml file with such list. 
+#' Colors should be given as a list where the first element is a vector of colors and the second element is a vector of breaks. 
+#' If only colors are given breaks would be implicitly determined from the minimum and maximum of the metadata field. 
 #'
 #'
 #' @examples
@@ -84,6 +85,26 @@ import_dataset <- function(project,
 
     mc_sum <- colSums(mc_mat)
     serialize_shiny_data(mc_sum, "mc_sum", dataset = dataset, cache_dir = cache_dir)
+
+    metadata <- load_metadata(metadata, metadata_fields, adata)
+    if (!is.null(metadata)){
+        serialize_shiny_data(
+            metadata %>% select(metacell, everything()),
+            "metadata",
+            dataset = dataset,
+            cache_dir = cache_dir,
+            flat = TRUE
+        )
+    }    
+
+    if (!is.null(metadata_colors)){        
+        cli_alert_info("Processing metadata colors")
+        if (is.character(metadata_colors)) {
+            metadata_colors <- yaml::read_yaml(metadata_colors) %>% as_tibble()
+        }
+        metadata_colors <- parse_metadata_colors(metadata_colors, metadata)
+        serialize_shiny_data(metadata_colors, "metadata_colors", dataset = dataset, cache_dir = cache_dir)
+    }        
 
     cli_alert_info("Processing 2d projection")
     graph <- Matrix::summary(adata$obsp$obs_outgoing_weights) %>%
@@ -217,151 +238,6 @@ dataset_rm <- function(project, dataset) {
 #' @export
 dataset_ls <- function(project) {
     basename(fs::dir_ls(project_cache_dir(project), type = c("directory", "symlink")))
-}
-
-#' Update cell type assignment for each metacell
-#'
-#' Change the cell type assignments for each metacell to the ones listed at \code{metacell_types_file}.
-#'
-#' This is usually done after a first iteration of annotation using the "Annotate" tab in the MCView annotation, which can export a valid \code{metacell_types_file}.
-#' The file should have a column named "metacell" with the metacell ids and another
-#' column named "cell_type" or "cluster" with the cell type assignment.
-#'
-#' Note that the exported file from the __MCView__ app contains additional fields
-#' which will be ignored in this function.
-#'
-#' Under the hood - MCView updates a file named "metacell_types.tsv" under \code{project/cache/dataset}, which can also be edited manually.
-#'
-#' @param project path to the project directory
-#' @param dataset name for the dataset, e.g. "PBMC"
-#' @param metacell_types_file path to a tabular file (csv,tsv) with cell type assignement for
-#' each metacell. The file should have a column named "metacell" with the metacell ids and another
-#' column named "cell_type" or "cluster" with the cell type assignment. Metacell ids that do
-#' not exists in the data would be ignored.
-#'
-#' @export
-#'
-#' @examples
-#' \dontrun{
-#' update_metacell_types("PBMC", "PBMC163k", "raw/metacell-clusters.csv")
-#' }
-#'
-#' @export
-update_metacell_types <- function(project, dataset, metacell_types_file) {
-    verify_project_dir(project)
-    verify_app_cache(project)
-
-    prev_metacell_types <- load_shiny_data("metacell_types", dataset, project_cache_dir(project))
-    prev_metacell_types <- prev_metacell_types %>%
-        mutate(metacell = as.character(metacell))
-
-    metacell_types <- parse_metacell_types(metacell_types_file)
-
-    metacell_types <- prev_metacell_types %>%
-        select(-cell_type) %>%
-        left_join(metacell_types %>% select(metacell, cell_type), by = "metacell")
-
-    serialize_shiny_data(metacell_types, "metacell_types", dataset = dataset, cache_dir = project_cache_dir(project), flat = TRUE)
-
-    cli_alert_success("Succesfully changed metacell cell type assignments")
-}
-
-
-#' Update color assignment for each cell type
-#'
-#' Change the color assignments for each cell type to the ones listed at \code{cell_type_colors_file}.
-#'
-#' This is usually done after a first iteration of annotation using the "Annotate" tab in the MCView annotation, which can
-#' export a valid \code{cell_type_colors_file}.
-#'
-#' The file should have a column named "cell_type" or "cluster" with the cell types and another column named "color" with the color assignment.
-#' Note that the exported file from the __MCView__ app contains additional fields which will be
-#' ignored in this function.
-#'
-#' Under the hood - MCView updates a file named "cell_type_colors.tsv" under \code{project/cache/dataset}, which can also be edited manually.
-#'
-#' @param project path to the project directory
-#' @param dataset name for the dataset, e.g. "PBMC"
-#' @param cell_type_colors_file path to a tabular file (csv,tsv) with color assignement for
-#' each cell type. The file should have a column named "cell_type" or "cluster" with the
-#' cell types and another column named "color" with the color assignment. Cell types that do not
-#' exist in the metacell types would be ignored, so if you changed the names of cell types you would have to also
-#' update the metacell types (using \code{update_metacell_types})
-#' If this parameter is missing, MCView would use the \code{chameleon} package to assign a color for each cell type.
-#'
-#'
-#' @examples
-#' \dontrun{
-#' update_metacell_types("PBMC", "PBMC163k", "raw/cluster-colors.csv")
-#' }
-#'
-#' @export
-update_cell_type_colors <- function(project, dataset, cell_type_colors_file) {
-    verify_project_dir(project)
-    verify_app_cache(project)
-
-    cell_type_colors <- parse_cell_type_colors(cell_type_colors_file)
-
-    serialize_shiny_data(cell_type_colors, "cell_type_colors", dataset = dataset, cache_dir = project_cache_dir(project), flat = TRUE)
-
-    cli_alert_success("Succesfully changed cell type color assignments")
-}
-
-
-parse_cell_type_colors <- function(file) {
-    cell_type_colors <- fread(file) %>% as_tibble()
-
-    if (!has_name(cell_type_colors, "cell_type") && !has_name(cell_type_colors, "cluster")) {
-        cli_abort("{.field {file}} should have a column named {.field cell_type} or {.field cluster}")
-    }
-
-    if (!has_name(cell_type_colors, "color")) {
-        cli_abort("{.field {file}} should have a column named {.field color}")
-    }
-
-    if (rlang::has_name(cell_type_colors, "cluster")) {
-        cell_type_colors <- cell_type_colors %>% rename(cell_type = cluster)
-    }
-
-    if (!has_name(cell_type_colors, "order")) {
-        cell_type_colors <- cell_type_colors %>% mutate(order = 1:n())
-    }
-
-    cell_type_colors <- cell_type_colors %>%
-        select(cell_type, color, order)
-
-    return(cell_type_colors)
-}
-
-
-parse_metacell_types <- function(file) {
-    metacell_types <- fread(file) %>% as_tibble()
-
-    if (!has_name(metacell_types, "metacell")) {
-        cli_abort("{.field {file}} should have a column named {.field metacell}")
-    }
-
-    if (!has_name(metacell_types, "cell_type") && !has_name(cell_type_colors, "cluster")) {
-        cli_abort("{.field {file}} should have a column named {.field cell_type} or {.field cluster}")
-    }
-
-    if (rlang::has_name(metacell_types, "cluster")) {
-        metacell_types <- metacell_types %>% rename(cell_type = cluster)
-    }
-
-    metacell_types <- metacell_types %>%
-        select(any_of(c("metacell", "cell_type", "age", "mc_age")))
-
-
-    if ("age" %in% colnames(metacell_types)) {
-        metacell_types <- metacell_types %>%
-            rename(mc_age = age)
-    }
-
-    metacell_types <- metacell_types %>%
-        mutate(metacell = as.character(metacell))
-
-    return(metacell_types)
 }
 
 #' calculate the k top correlated and anti correlated genes for each gene
--- R/metadata.R
+++ R/metadata.R
@@ -1,6 +1,212 @@
+#' Update metadata for a dataset
+#' 
+#' 
+#' @param overwrite overwrite all existing metadata. If \code{FALSE} - would override only existing metadata fields. 
+#' 
+#' 
+#' @inheritParams import_dataset
+#' 
+#' @export
+update_metadata <- function(
+    project, 
+    dataset,         
+    metadata=NULL,     
+    metadata_fields=NULL,
+    anndata_file = NULL, 
+    overwrite = FALSE){
+    
+    cache_dir <- project_cache_dir(project)
+
+    if (!is.null(metadata_fields)){
+        if (is.null(anndata_file)){
+            cli_abort("You have to provide the {.code anndata_file} parameter with {.code metadata_fields}")
+        } else {
+            if (!fs::file_exists(anndata_file)) {
+                cli_abort("{anndata_file} doesn't exist. Maybe there is a typo?")
+            }
+
+            cli_alert_info("Reading {.file {anndata_file}}")
+            library(anndata)
+            adata <- anndata::read_h5ad(anndata_file)
+        }
+    }
+
+    cli_alert_info("Processing metadata")
+    metadata <- load_metadata(metadata, metadata_fields, adata)    
+
+    prev_metadata_file <- fs::path(cache_dir, dataset, "metadata.tsv")
+    if (fs::file_exists(prev_metadata_file)){
+        cli_alert_info("Merging with previous metadata")
+        prev_metadata <- fread(prev_metadata_file) %>% as_tibble()
+        metadata <- metadata %>%
+            select(-any_of(colnames(prev_metadata)[-1])) %>%
+            left_join(prev_metadata, by = "metacell")
+    }       
+
+
+    serialize_shiny_data(
+        metadata %>% select(metacell, everything()), 
+        "metadata",
+        dataset = dataset, 
+        cache_dir = cache_dir, 
+        flat = TRUE
+    )
+
+    cli_alert_success("Successfully updated metadata of dataset {.field {dataset}} at {.path {project}} project")
+}
+
+#' Update metadata for a dataset
+#'
+#'
+#' @param overwrite overwrite all existing colors. If \code{FALSE} - would override only 
+#' the colors of existing metadata fields.
+#'
+#'
+#' @inheritParams import_dataset
+#'
+#' @export
+update_metadata_colors <- function(project,
+                            dataset,
+                            metadata_colors,                            
+                            overwrite = FALSE) {
+    cache_dir <- project_cache_dir(project)
+
+    cli_alert_info("Processing metadata colors")       
+    if (is.character(metadata_colors)) {
+        metadata_colors <- yaml::read_yaml(metadata_colors) %>% as_tibble()
+    }
+
+    new_metadata_colors <- parse_metadata_colors(metadata_colors, metadata)
+
+    prev_colors_file <- fs::path(cache_dir, dataset, "metadata_colors.qs")
+    if (fs::file_exists(prev_metadata_file)) {
+        metadata_colors <- qs::qread(prev_colors_file)
+        for (f in names(metadata_colors)) {
+            metadata_colors[[f]] <- NULL
+        }
+        metadata_colors <- c(metadata_colors, new_metadata_colors)
+    } else {
+        metadata_colors <- new_metadata_colors
+    }
+    
+    serialize_shiny_data(metadata_colors, "metadata_colors", dataset = dataset, cache_dir = cache_dir)        
+}
+
+
+
 #' Convert cell metadata to metacell metadata
 #'
 #' @export
 cell_metadata_to_metacell <- function() {
 
 }
+
+
+
+load_metadata <- function(metadata, metadata_fields, metacells, adata){
+    metadata_df <- NULL
+    if (!is.null(metadata)){        
+        if (is.character(metadata)){
+            metadata <- tgutil::fread(metadata) %>% as_tibble()
+        }
+        metadata_df <- parse_metadata(metadata, metacells)
+    }
+    
+    if (!is.null(metadata_fields)){
+        purrr::walk(metadata_fields, ~ {
+            if (!(.x %in% colnames(adata$obs))) {
+                cli_abort("{.field {.x}} is not present in the h5ad object.")
+            }
+        })        
+        
+        metadata_df_obs <- adata$obs %>% select(one_of(metadata_fields)) %>% rownames_to_column("metacell")
+        metadata_df_obs <- parse_metadata(metadata_df_obs, metacells)
+
+        if (!is.null(metadata_df)) {
+            duplicated_metadata <- intersect(colnames(metadata), colnames(metadata_df))
+            duplicated_metadata <- duplicated_metadata[duplicated_metadata != "metacell"]
+            if (length(duplicated_metadata) > 0) {
+                cli_abort("The metadata fields {.field {fields}} appear both in {.code metadata} and {.code metadata_fields}", fields = paste(duplicated_metadata, collapse = ", "))
+            }
+
+            metadata <- metadata_df_obs %>% full_join(metadata_df, by = "metacell")
+        } else {
+            metadata <- metadata_df_obs
+        }
+    } else {
+        metadata <- metadata_df
+    } 
+
+    return(metadata)
+}
+
+parse_metadata <- function(metadata, metacells){
+    metadata <- metadata %>% as_tibble()
+
+    if (!has_name(metadata_colors, "metacell")){
+        cli_abort("metadata doesn't have a field named {.field metacell}")
+    }
+
+    unknown_metacells <- metadata$metacell[!(metadata$metacell %in% metacells)]
+    if (length(unknown_metacells) > 0){
+        cli_abort("Metadata contains metacells that are missing from the data: {.field {mcs}}", mcs = paste(unknown_metacells, collapse = ", "))
+    }
+
+    missing_metacells <- metacells[!(metacells %in% metadata$metacell)]
+    if (length(missing_metacells) > 0) {
+        cli_warning("Some metacells are missing from metadata: {.field {mcs}}", mcs = paste(missing_metacells, collapse = ", "))
+    }
+
+    fields <- metadata %>%
+        select(-metacell) %>%
+        colnames()
+
+    metadata <- metadata %>%
+        mutate_at(one_of(fields), as.numeric)
+    
+    purrr::walk(fields, ~ {
+        if (all(is.na(metadata[[.x]]))){
+            cli_abort("The metadata variable {.field {.x}} is all NA. Is it numeric? Convert categorical variables to numeric using {.code cell_metadata_to_metacell} with {.code categorical=TRUE}")
+        }
+    })
+
+    return(metadata)
+}
+
+parse_metadata_colors <- function(metadata_colors, metadata) {
+    if (is.null(metadata)){
+        cli_abort("{.code metadata_colors} is set but {.code metadata} is {.code NULL}. Did you forget to set the {.code metadata} parameter?")
+    }
+
+    metadata_colors <- purrr::imap(metadata_colors, ~ {
+        if (!has_name(metadata, .y)) {
+            cli_abort("{.code metadata} doesen't have a field named {.field {.y}} but it exists in {.code metadata_colors}")
+        }
+        
+        if (length(.x) == 0 || length(.x[[1]]) == 0) {
+            cli_abort("{.field {.y}} in {.code metadata_colors} does not contain any colors.")
+        }
+        
+        colors <- .x[[1]]
+        
+        if (length(.x) > 1){
+            breaks <- .x[[2]]
+            if (length(breaks) != length(colors)) {
+                cli_abort("In metadata colors field {.field {.y}}: length of {.code breaks} shuold be equal to {.code colors}")
+            }
+            return(list(
+                colors = colors,
+                breaks = breaks
+            ))
+        } else {
+            return(list(
+                colors = colors
+            ))
+        }
+    })
+
+    return(metadata_colors)
+}
+
+
+
