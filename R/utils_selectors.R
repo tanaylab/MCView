bc887a24b4b39fed510ad4ab6bb0fa15bbcf10d0
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2022-01-30 17:41:13 +0200

    fix: metacell_names is a variable in metacell_selector

--- R/utils_selectors.R
+++ R/utils_selectors.R
@@ -28,7 +28,7 @@ metacell_selector <- function(dataset, ns, id, label, selected = NULL, atlas = F
         metacell_names <- colnames(get_mc_data(dataset(), "mc_mat", atlas = atlas))
         shinyWidgets::pickerInput(ns(id), label,
             choices = metacell_names,
-            selected = selected %||% config$selected_mc1 %||% metacell_names()[1],
+            selected = selected %||% config$selected_mc1 %||% metacell_names[1],
             multiple = FALSE,
             options = shinyWidgets::pickerOptions(liveSearch = TRUE, liveSearchNormalize = TRUE, liveSearchStyle = "startsWith", ...)
         )
