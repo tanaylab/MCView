6cb7c3c5a2b48fea59a061513f10c2c08bd00b6a
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2022-05-03 09:24:05 +0300

    Added remove button to clipboard modal

--- R/app_server.R
+++ R/app_server.R
@@ -192,36 +192,7 @@ app_server <- function(input, output, session) {
         show_help(input, output, session)
     )
 
-    observeEvent(
-        input$clipboard_modal,
-        showModal(modalDialog(
-            title = "Clipboard",
-            actionButton("clear_clipboard", "Clear clipboard"),
-            br(),
-            br(),
-            shinycssloaders::withSpinner(
-                DT::dataTableOutput("clipboard_table")
-            ),
-            easyClose = TRUE
-        ))
-    )
-
-    observeEvent(input$clear_clipboard, {
-        globals$clipboard <- character(0)
-    })
-
-    output$clipboard_table <- DT::renderDataTable(
-        metacell_types() %>% filter(metacell %in% globals$clipboard) %>% select(metacell, cell_type),
-        escape = FALSE,
-        server = FALSE,
-        rownames = FALSE,
-        filter = "top",
-        options = list(
-            dom = "t",
-            paging = FALSE,
-            language = list(emptyTable = "No metacells in clipboard")
-        )
-    )
+    clipboard_reactives(dataset, input, output, session, metacell_types, cell_type_colors, gene_modules, globals)
 
     observeEvent(
         input$download_modal,
--- R/utils_clipboard.R
+++ R/utils_clipboard.R
@@ -18,3 +18,41 @@ clipboard_changed_scatter_reactive <- function(input, globals) {
         }
     })
 }
+
+clipboard_reactives <- function(dataset, input, output, session, metacell_types, cell_type_colors, gene_modules, globals){
+    observeEvent(
+        input$clipboard_modal,
+        showModal(modalDialog(
+            title = "Clipboard",
+            actionButton("clear_clipboard", "Clear clipboard"),
+            actionButton("delete_clipboard_row", "Remove selected"),
+            br(),
+            br(),
+            shinycssloaders::withSpinner(
+                DT::dataTableOutput("clipboard_table")
+            ),
+            easyClose = TRUE
+        ))
+    )
+
+    observeEvent(input$clear_clipboard, {
+        globals$clipboard <- character(0)
+    })
+
+    observeEvent(input$delete_clipboard_row, {
+        globals$clipboard <- globals$clipboard[-input$clipboard_table_rows_selected]
+    })
+
+    output$clipboard_table <- DT::renderDataTable(
+        metacell_types() %>% filter(metacell %in% globals$clipboard) %>% select(metacell, cell_type),
+        escape = FALSE,
+        server = FALSE,
+        rownames = FALSE,
+        filter = "top",
+        options = list(
+            dom = "t",
+            paging = FALSE,
+            language = list(emptyTable = "No metacells in clipboard")
+        )
+    )
+}
