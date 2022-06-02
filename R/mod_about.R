232a934284c3a2f78bd4df4ae2cd2e8a244a50f4
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2022-06-02 11:53:51 +0300

    Updated default "about" tab

--- R/mod_about.R
+++ R/mod_about.R
@@ -10,8 +10,7 @@
 mod_about_ui <- function(id) {
     ns <- NS(id)
     tagList(
-        includeRMarkdown(about_file) # ,
-        # plotOutput(ns("plot_manifold_proj_2d"))
+        includeRMarkdown(about_file)
     )
 }
 
@@ -39,19 +38,6 @@ mod_about_server <- function(id, dataset, metacell_types, cell_type_colors, gene
         id,
         function(input, output, session) {
             ns <- session$ns
-
-            output$plot_manifold_proj_2d <- renderPlot({
-                req(metacell_types())
-                req(cell_type_colors())
-                mc2d_plot_ggp(
-                    dataset(),
-                    metacell_types = metacell_types(),
-                    cell_type_colors = cell_type_colors(),
-                    point_size = initial_proj_point_size(dataset(), globals$screen_width, globals$screen_height),
-                    stroke = initial_proj_stroke(dataset()),
-                    min_d = min_edge_length(dataset())
-                ) + theme(aspect.ratio = 1)
-            })
         }
     )
 }
