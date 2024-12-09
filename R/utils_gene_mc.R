20488cdd46a294b3917f9e1a79f6fa9b1d061b9d
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2024-12-09 09:56:04 +0200

    some code tidying

--- R/utils_gene_mc.R
+++ R/utils_gene_mc.R
@@ -21,63 +21,62 @@ get_cell_type_colors <- function(dataset, cell_type_colors = NULL, na_color = "g
     return(res)
 }
 
-get_top_cor_gene <- function(dataset, gene, type = "pos", atlas = FALSE, data_vec = NULL, exclude = NULL, metacell_filter = NULL) {
-    gg_mc_top_cor <- get_mc_data(dataset, "gg_mc_top_cor", atlas = atlas)
+get_cached_cor <- function(gg_mc_top_cor, gene, type, exclude = NULL) {
+    df <- gg_mc_top_cor %>%
+        filter(gene1 == gene)
+
+    if (type %in% c("pos", "neg")) {
+        df <- df %>%
+            filter(type == !!type)
+    }
+
+    df <- df %>%
+        filter(gene1 != gene2)
+
+    if (!is.null(exclude)) {
+        df <- df %>%
+            filter(!(gene2 %in% exclude))
+    }
+    return(df)
+}
+
+calc_top_cors <- function(dataset, gene, type, data_vec, metacell_filter, exclude, atlas = FALSE) {
+    mc_egc <- get_mc_egc(dataset, atlas = atlas)
+    req(gene %in% rownames(mc_egc))
+    lfp <- log2(mc_egc + egc_epsilon)
+    if (!is.null(exclude)) {
+        lfp <- lfp[-which(rownames(lfp) %in% exclude), , drop = FALSE]
+    }
 
     if (!is.null(data_vec)) {
         req(!atlas)
-        mc_egc <- get_mc_egc(dataset, atlas = FALSE)
-        lfp <- log2(mc_egc + egc_epsilon)
-        if (!is.null(exclude)) {
-            lfp <- lfp[-which(rownames(lfp) %in% exclude), , drop = FALSE]
-        }
         cors <- tgs_cor(as.matrix(data_vec[colnames(lfp)]), t(lfp), pairwise.complete.obs = TRUE, spearman = FALSE)[1, ]
-        if (type == "pos") {
-            cors <- head(sort(cors, decreasing = TRUE), n = 30)
-        } else if (type == "neg") {
-            cors <- head(sort(cors), n = 30)
-        } else {
-            cors <- c(head(sort(cors, decreasing = TRUE), n = 30), head(sort(cors), n = 30))
-        }
-        df <- enframe(cors, "gene2", "cor")
     } else {
-        if (gene %in% gg_mc_top_cor$gene1 && is.null(metacell_filter)) {
-            df <- gg_mc_top_cor %>%
-                filter(gene1 == gene)
-
-            if (type %in% c("pos", "neg")) {
-                df <- df %>%
-                    filter(type == !!type)
-            }
-
-            df <- df %>%
-                filter(gene1 != gene2)
-
-            if (!is.null(exclude)) {
-                df <- df %>%
-                    filter(!(gene2 %in% exclude))
-            }
-        } else {
-            mc_egc <- get_mc_egc(dataset, atlas = atlas)
-            req(gene %in% rownames(mc_egc))
-            lfp <- log2(mc_egc + egc_epsilon)
-            if (!is.null(metacell_filter)) {
-                req(metacell_filter %in% colnames(lfp))
-                lfp <- lfp[, metacell_filter, drop = FALSE]
-            }
-            if (!is.null(exclude)) {
-                lfp <- lfp[-which(rownames(lfp) %in% exclude), , drop = FALSE]
-            }
-            cors <- tgs_cor(t(as.matrix(lfp[gene, , drop = FALSE])), t(lfp), pairwise.complete.obs = TRUE, spearman = FALSE)[1, ]
-            if (type == "pos") {
-                cors <- head(sort(cors, decreasing = TRUE), n = 31)[-1]
-            } else if (type == "neg") {
-                cors <- head(sort(cors), n = 30)
-            } else {
-                cors <- c(head(sort(cors, decreasing = TRUE), n = 31)[-1], head(sort(cors), n = 30))
-            }
-            df <- enframe(cors, "gene2", "cor")
+        if (!is.null(metacell_filter)) {
+            req(metacell_filter %in% colnames(lfp))
+            lfp <- lfp[, metacell_filter, drop = FALSE]
         }
+        cors <- tgs_cor(t(as.matrix(lfp[gene, , drop = FALSE])), t(lfp), pairwise.complete.obs = TRUE, spearman = FALSE)[1, ]
+    }
+
+    if (type == "pos") {
+        cors <- head(sort(cors, decreasing = TRUE), n = 30)
+    } else if (type == "neg") {
+        cors <- head(sort(cors), n = 30)
+    } else {
+        cors <- c(head(sort(cors, decreasing = TRUE), n = 30), head(sort(cors), n = 30))
+    }
+    df <- enframe(cors, "gene2", "cor")
+
+    return(df)
+}
+
+get_top_cor_gene <- function(dataset, gene, type = "pos", atlas = FALSE, data_vec = NULL, exclude = NULL, metacell_filter = NULL) {
+    gg_mc_top_cor <- get_mc_data(dataset, "gg_mc_top_cor", atlas = atlas)
+    if (is.null(data_vec) && is.null(metacell_filter) && gene %in% gg_mc_top_cor$gene1) {
+        df <- get_cached_cor(gg_mc_top_cor, gene, type, exclude = exclude)
+    } else {
+        df <- calc_top_cors(dataset, gene, type, data_vec, metacell_filter, exclude, atlas = atlas)
     }
 
     res <- df %>%
