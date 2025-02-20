9b2e6056ac3b65962c861674924d8f62dbb9130a
Tal Sokolov <talso@n109.mcl4.weizmann.ac.il>
2025-02-20 10:49:20 +0200

    debug id

--- R/mod_st_flow.R
+++ R/mod_st_flow.R
@@ -12,13 +12,10 @@ mod_st_flow_ui <- function(id) {
     tagList(
         generic_column(
             width = 3,
-            projection_box(ns, "gene_projection1", title = "2D Projection", collapsed_accordion = FALSE, show_legend = FALSE, 
-                           color_choices = c("Flow in", "Flow out", "Cell type"), plotly_height = "30vh", height = "30vh")
-        ),
-        generic_column(
-            width = 3,
-            projection_box(ns, "gene_projection2", title = "2D Projection", collapsed_accordion = FALSE, show_legend = FALSE, 
-                           color_choices = c("Flow in", "Flow out", "Cell type"), plotly_height = "30vh", height = "30vh")
+            # projection_box_id(ns, "gene_projection1", title = "2D Projection", collapsed_accordion = FALSE, show_legend = FALSE, 
+            #                color_choices = c("Cell type", "Flow in", "Flow out"), plotly_height = "30vh", height = "30vh")
+            projection_box(ns, "flow_proj", title = "2D Projection", collapsed_accordion = FALSE, show_legend = FALSE, 
+                           color_choices = c("Cell type", "Flow in", "Flow out"), plotly_height = "30vh", height = "30vh")
         )
     )
 }
@@ -81,7 +78,7 @@ mod_st_flow_server <- function(id, dataset, metacell_types, cell_type_colors, ge
             metacell_colors <- metacell_colors_reactive(dataset, metacell_names, metacell_types)
             display_selectors(input, output, session, dataset, ns, metacell_names, metacell_colors, metacell_types, cell_type_colors)
 
-            projection_selectors(ns, dataset, output, input, gene_modules, globals, session)
+            # projection_selectors_id(ns, 'gene_projection1', dataset, output, input, gene_modules, globals, session)
             projection_selectors(ns, dataset, output, input, gene_modules, globals, session)
 
             data <- get_mc_data(dataset(), "spatial_flow_data")
@@ -99,7 +96,6 @@ mod_st_flow_server <- function(id, dataset, metacell_types, cell_type_colors, ge
             })
 
             # Projection plots
-            output$plot_gene_proj_2d <- render_2d_plotly_temporal(input, output, session, dataset, data, time_bin = '8', metacell_types, metacell_names, cell_type_colors, gene_modules, globals, source = "proj_manifold_plot")
             output$plot_gene_proj_2d <- render_2d_plotly_temporal(input, output, session, dataset, data, time_bin = '9', metacell_types, metacell_names, cell_type_colors, gene_modules, globals, source = "proj_manifold_plot")
 
         }
@@ -138,28 +134,38 @@ render_2d_plotly_temporal <- function(input, output, session, dataset, data, tim
                 mc2d = mc2d,
                 selected_cell_types = selected_cell_types)
 
-        }else if(proj_opt == 'Flow in'){
+        }else if(proj_opt == 'Flow in' | proj_opt == 'Flow out'){
 
             req(metacell_names())
             smcs = metacell_names()
 
             req(input$display_select %in% metacell_names())
             smc = input$display_select
-            flow_to = data$flow_to
-            source_smcs = flow_to[flow_to$smc2 == smc & flow_to$time_bin == time_bin,]
-
-            colfunc <- colorRampPalette(c("white", "Red"))
-            colfunc(100)
-
-            flow_color = rep('#ffffff', length(smcs))
-            names(flow_color) = smcs 
-            flow_color[smc] = '#000000'
-            flow_color[source_smcs$smc1] = colfunc(100)[(round(source_smcs$f_norm,2)*100) + 1]
 
             flow_stroke = rep(initial_scatters_stroke(dataset), length(smcs))
             names(flow_stroke) = smcs
             flow_stroke[smc] = initial_scatters_stroke(dataset)*3
 
+            flow_color = rep('#ffffff', length(smcs))
+            names(flow_color) = smcs 
+            flow_color[smc] = '#000000'
+
+            if(proj_opt == 'Flow in'){
+
+                colgrad <- colorRampPalette(c("white", "Red"))
+
+                flow_to = data$flow_to
+                source_smcs = flow_to[flow_to$smc2 == smc & flow_to$time_bin == time_bin,]
+                flow_color[source_smcs$smc1] = colgrad(100)[(round(source_smcs$f_norm,2)*100) + 1]
+            }else{
+
+                colgrad <- colorRampPalette(c("white", "Blue"))
+
+                flow_from = data$flow_from
+                target_smcs = flow_from[flow_from$smc1 == smc & flow_from$time_bin == time_bin,]
+                flow_color[target_smcs$smc2] = colgrad(100)[(round(target_smcs$f_norm,2)*100) + 1]
+            }
+            
             fig <- mc2d_plot_metadata_ggp(
                 dataset(),
                 "metacell",
@@ -167,7 +173,7 @@ render_2d_plotly_temporal <- function(input, output, session, dataset, data, tim
                 min_d = input$min_edge_size,
                 metacell_types = metacell_types(),
                 atlas = atlas,
-                metadata = metacell_types() %>% rename(`Cell type` = cell_type),
+                # metadata = metacell_types() %>% rename(`Cell type` = cell_type),
                 colors = flow_color,
                 stroke = flow_stroke,
                 graph_name = input$graph_name,
@@ -263,3 +269,249 @@ display_selectors <- function(input, output, session, dataset, ns, metacell_name
             )
         }
     })}
+
+projection_box_id <- function(ns,
+                           id,
+                           ...,
+                           color_choices = c("Cell type", "Gene", "Gene module", "Metadata"),
+                           title = "2D Projection",
+                           height = NULL,
+                           plotly_height = "400px",
+                           additional_elements = NULL,
+                           collapsed_accordion = TRUE,
+                           legend_orientation = "Vertical",
+                           show_legend = TRUE) {
+    generic_box(
+        id = ns(id),
+        title = title,
+        status = "primary",
+        solidHeader = TRUE,
+        collapsible = TRUE,
+        closable = FALSE,
+        width = 12,
+        height = height,
+        sidebar = shinydashboardPlus::boxSidebar(
+            startOpen = FALSE,
+            width = 80,
+            id = ns(glue("{id}_sidebar")),
+            uiOutput(ns(glue("{id}_graph_select"))),
+            uiOutput(ns(glue("{id}_proj_stat"))),
+            uiOutput(ns(glue("{id}_set_range"))),
+            uiOutput(ns(glue("{id}_expr_range"))),
+            uiOutput(ns(glue("{id}_enrich_range"))),
+            uiOutput(ns(glue("{id}_point_size"))),
+            uiOutput(ns(glue("{id}_stroke"))),
+            uiOutput(ns(glue("{id}_edge_distance"))),
+            shinyWidgets::prettyRadioButtons(
+                ns(glue("{id}_legend_orientation")),
+                label = "Legend orientation:",
+                choices = c("Vertical", "Horizontal"),
+                selected = legend_orientation,
+                inline = TRUE,
+                status = "danger",
+                fill = TRUE
+            )
+        ),
+        shinycssloaders::withSpinner(
+            plotly::plotlyOutput(ns(glue("{id}_plot_gene_proj_2d")), height = plotly_height)
+        ),
+        shinydashboardPlus::accordion(
+            id = ns(glue("{id}_proj_accordion")),
+            shinydashboardPlus::accordionItem(
+                title = "Modify colors",
+                collapsed = collapsed_accordion,
+                shinyWidgets::prettyRadioButtons(
+                    ns(glue("{id}_color_proj")),
+                    label = "Color by:",
+                    choices = color_choices,
+                    inline = TRUE,
+                    status = "danger",
+                    fill = TRUE
+                ),
+                ...,
+                shinyWidgets::virtualSelectInput(
+                    ns(glue("{id}_color_proj_gene")),
+                    "Gene:",
+                    choices = c(),
+                    multiple = FALSE,
+                    search = TRUE,
+                    dropboxWrapper = "body",
+                    markSearchResults = TRUE,
+                    searchByStartsWith = TRUE
+                ),
+                shinyWidgets::virtualSelectInput(
+                    ns(glue("{id}_color_proj_metadata")),
+                    "Metadata:",
+                    choices = c(),
+                    multiple = FALSE,
+                    search = TRUE,
+                    dropboxWrapper = "body"
+                ),
+                shinyWidgets::virtualSelectInput(
+                    ns(glue("{id}_color_proj_gene_module")),
+                    "Gene module:",
+                    choices = c(),
+                    multiple = FALSE,
+                    search = TRUE,
+                    dropboxWrapper = "body"
+                ),
+                shinyWidgets::prettyRadioButtons(
+                    ns(glue("{id}_scatter_axis_proj")),
+                    label = "Axis:",
+                    choices = c("x", "y"),
+                    inline = TRUE,
+                    status = "danger",
+                    fill = TRUE
+                ),
+                checkboxInput(ns(glue("{id}_show_legend_projection")), "Show legend", value = show_legend)
+            )
+        )
+    )
+}
+
+projection_selectors_id <- function(ns, id, dataset, output, input, gene_modules, globals, session, weight = 1, atlas = FALSE) {
+    observe({
+        shinyWidgets::updateVirtualSelect(
+            session = session,
+            inputId = glue("{id}_color_proj_gene"),
+            choices = gene_names_label(dataset(), atlas = atlas),
+            selected = default_gene1
+        )
+
+        shinyWidgets::updateVirtualSelect(
+            session = session,
+            inputId = glue("{id}_color_proj_metadata"),
+            choices = c("Clipboard", dataset_metadata_fields(dataset(), atlas = atlas)),
+            selected = dataset_metadata_fields(dataset(), atlas = atlas)[1]
+        )
+
+        shinyWidgets::updateVirtualSelect(
+            session = session,
+            inputId = glue("{id}_color_proj_gene_module"),
+            choices = levels(gene_modules()$module),
+            selected = NULL
+        )
+    })
+
+    picker_options <- shinyWidgets::pickerOptions(liveSearch = TRUE, liveSearchNormalize = TRUE, liveSearchStyle = "contains", dropupAuto = FALSE)
+
+    observe({
+        req(input[[glue("{id}_color_proj")]])
+        shinyjs::toggle(id = glue("{id}_color_proj_gene"), condition = input[[glue("{id}_color_proj")]] == "Gene")
+        shinyjs::toggle(id = glue("{id}_color_proj_metadata"), condition = input[[glue("{id}_color_proj")]] == "Metadata")
+        shinyjs::toggle(id = glue("{id}_color_proj_gene_module"), condition = input[[glue("{id}_color_proj")]] == "Gene module")
+        shinyjs::toggle(id = glue("{id}_scatter_axis_proj"), condition = input[[glue("{id}_color_proj")]] == "Scatter Axis")
+    })
+
+    output[[glue("{id}_proj_stat")]] <- renderUI({
+        req(input[[glue("{id}_color_proj")]] == "Gene" ||
+            input[[glue("{id}_color_proj")]] == "Gene A" ||
+            input[[glue("{id}_color_proj")]] == "Gene B" ||
+            input[[glue("{id}_color_proj")]] == "Gene module")
+        selectInput(ns(glue("{id}_proj_stat")),
+            label = "Statistic",
+            choices = c("Expression" = "expression", "Enrichment" = "enrichment"),
+            selected = "Expression",
+            multiple = FALSE,
+            selectize = FALSE
+        )
+    })
+
+    output[[glue("{id}_graph_select")]] <- renderUI({
+        choices <- c("metacell")
+        graphs <- get_mc_data(dataset(), "metacell_graphs")
+        if (!is.null(graphs)) {
+            choices <- c(choices, names(graphs))
+        }
+        selectInput(ns(glue("{id}_graph_name")),
+            label = "Graph",
+            choices = choices,
+            selected = "metacell",
+            multiple = FALSE,
+            selectize = FALSE
+        )
+    })
+
+    output[[glue("{id}_set_range")]] <- renderUI({
+        req(input[[glue("{id}_color_proj")]] == "Gene" ||
+            input[[glue("{id}_color_proj")]] == "Gene A" ||
+            input[[glue("{id}_color_proj")]] == "Gene B" ||
+            input[[glue("{id}_color_proj")]] == "Gene module")
+        req(input[[glue("{id}_proj_stat")]] == "expression")
+        checkboxInput(ns(glue("{id}_set_range")), "Manual range", value = FALSE)
+    })
+
+    output[[glue("{id}_expr_range")]] <- renderUI({
+        req(input[[glue("{id}_color_proj")]] == "Gene" ||
+            input[[glue("{id}_color_proj")]] == "Gene A" ||
+            input[[glue("{id}_color_proj")]] == "Gene B" ||
+            input[[glue("{id}_color_proj")]] == "Gene module")
+        req(input[[glue("{id}_proj_stat")]] == "expression")
+        req(input[[glue("{id}_set_range")]])
+        shinyWidgets::numericRangeInput(ns(glue("{id}_expr_range")),
+            "Expression range",
+            c(-18, -5),
+            width = "80%",
+            separator = " to "
+        )
+    })
+
+    output[[glue("{id}_enrich_range")]] <- renderUI({
+        req(input[[glue("{id}_color_proj")]] == "Gene" ||
+            input[[glue("{id}_color_proj")]] == "Gene A" ||
+            input[[glue("{id}_color_proj")]] == "Gene B" ||
+            input[[glue("{id}_color_proj")]] == "Gene module")
+        req(input[[glue("{id}_proj_stat")]] == "enrichment")
+        shinyWidgets::numericRangeInput(ns(glue("{id}_lfp")),
+            "Enrichment range",
+            c(-3, 3),
+            width = "80%",
+            separator = " to "
+        )
+    })
+
+    output[[glue("{id}_point_size")]] <- renderUI({
+        req(globals$screen_height)
+        req(globals$screen_width)
+        req(dataset())
+        numericInput(ns(glue("{id}_point_size")),
+            label = "Point size",
+            value = initial_proj_point_size(dataset(), globals$screen_width, globals$screen_height, weight = weight, atlas = atlas),
+            min = 0.1,
+            max = 3,
+            step = 0.1
+        )
+    })
+
+    output[[glue("{id}_stroke")]] <- renderUI({
+        numericInput(ns(glue("{id}_stroke")),
+            label = "Stroke width",
+            value = initial_proj_stroke(dataset()),
+            min = 0,
+            max = 3,
+            step = 0.01
+        )
+    })
+
+    output[[glue("{id}_edge_distance")]] <- renderUI({
+        graph <- input[[glue("{id}_graph_name")]]
+        if (is.null(graph) || graph == "metacell") {
+            sliderInput(ns(glue("{id}_min_edge_size")),
+                label = "Min edge length",
+                min = 0,
+                max = 0.3,
+                value = min_edge_length(dataset()),
+                step = 0.001
+            )
+        } else {
+            graph <- get_mc_data(dataset(), "metacell_graphs")[[graph]]
+            sliderInput(ns(glue("{id}_min_edge_size")),
+                label = "Min weight",
+                min = min(graph$weight, na.rm = TRUE),
+                max = max(graph$weight, na.rm = TRUE),
+                value = median(graph$weight),
+                step = (max(graph$weight, na.rm = TRUE) - min(graph$weight, na.rm = TRUE)) / 50
+            )
+        }
+    })
+}
